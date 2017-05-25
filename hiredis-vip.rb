917fe56bf31c453a04a2f3dbcdf44beb6f0de68f
Cubic <hourui.me@gmail.com>
2017-05-25 17:25:47 +0800

    update to newest version

--- hiredis-vip.rb
+++ hiredis-vip.rb
@@ -1,8 +1,8 @@
 class HiredisVip < Formula
   desc "Minimalistic client for Redis Cluster vip"
   homepage "https://github.com/vipshop/hiredis-vip"
-  url "https://github.com/vipshop/hiredis-vip/archive/0.3.0.tar.gz"
-  sha256 "84e0f9367fa25089fc073b7a8a0725043c48cccec827acf4555a63da68f36be5"
+  url "https://github.com/vipshop/hiredis-vip/archive/f12060498004494a3e1de11f653a8624f3d218c3.tar.gz"
+  sha256 "5774ac621aebfc9c2d622e967696b7cef61e2a068dbda0dc15bd04bf3d51e183"
 
   head "https://github.com/vipshop/hiredis-vip.git"
 
@@ -22,20 +22,4 @@ class HiredisVip < Formula
     File.exist? testpath/"test"
   end
 
-  patch :DATA
 end
-
-__END__
-diff --git a/Makefile b/Makefile
-index 58494bf..fa33b8e 100644
---- a/Makefile
-+++ b/Makefile
-@@ -7,7 +7,7 @@ OBJ=net.o hiredis.o sds.o async.o read.o hiarray.o hiutil.o command.o crc16.o ad
- EXAMPLES=hiredis-example hiredis-example-libevent hiredis-example-libev hiredis-example-glib
- TESTS=hiredis-test
- LIBNAME=libhiredis_vip
--PKGCONFNAME=hiredis.pc
-+PKGCONFNAME=hiredis_vip.pc
-
- HIREDIS_VIP_MAJOR=$(shell grep HIREDIS_VIP_MAJOR hircluster.h | awk '{print $$3}')
- HIREDIS_VIP_MINOR=$(shell grep HIREDIS_VIP_MINOR hircluster.h | awk '{print $$3}')
